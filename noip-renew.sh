530fb42eea0a439d7ac590704aea727832a6b9c0
Sergio García Villalonga <yayalose@protonmail.com>
2020-02-01 10:18:49 +0100

    Removes log message

--- noip-renew.sh
+++ noip-renew.sh
@@ -7,7 +7,6 @@ LOGDIR=$1
 PROGDIR=$(dirname $0)
 
 if [ -z "$LOGDIR" ]; then
-    echo Executing "$PROGDIR/noip-renew.py "$USERNAME" "$PASSWORD""
     $PROGDIR/noip-renew.py "$USERNAME" "$PASSWORD"
 else
     cd $LOGDIR
