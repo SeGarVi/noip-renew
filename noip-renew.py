abdadfeea1413d4b3b3146ddaa243b9b17bbffd0
Sergio García Villalonga <yayalose@protonmail.com>
2020-02-16 22:16:14 +0100

    Sends message through a Telegram bot when finished

--- noip-renew.py
+++ noip-renew.py
@@ -19,6 +19,34 @@ import time
 import sys
 import os
 import re
+import telegram_send
+import importlib
+
+
+class ClassByNameBuilder:
+    @staticmethod
+    def load(module_name, class_name):
+        module = importlib.import_module(module_name)
+        class_ = getattr(module, class_name)
+        instance = class_()
+        return instance
+
+
+class TelegramSender:
+    @staticmethod
+    def send_with_image(message, image_path):
+        with open(image_path, "rb") as f:
+            telegram_send.send(captions=[message], images=[f])
+
+
+class Logger:
+    def __init__(self, level):
+        self.time_string_formatter = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time()))
+        self.level = 0 if level is None else level
+
+    def log(self, msg):
+        if self.level > 0:
+            print("[%s]: %s" % (self.time_string_formatter, msg))
 
 
 class Robot:
@@ -26,11 +54,12 @@ class Robot:
     LOGIN_URL = "https://www.noip.com/login"
     HOST_URL = "https://my.noip.com/#!/dynamic-dns"
 
-    def __init__(self, username, password, debug=0):
+    def __init__(self, username, password, debug):
         self.debug = debug
         self.username = username
         self.password = password
         self.browser = self.init_browser()
+        self.logger = Logger(debug)
 
     @staticmethod
     def init_browser():
@@ -49,25 +78,18 @@ class Robot:
         browser.set_page_load_timeout(60)
         return browser
 
-    def log_msg(self, msg, level=None):
-        time_string_formatter = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time()))
-        if level is None:
-            level = self.debug
-        if level > 0:
-            print("%s [%s] - %s" % (time_string_formatter, self.username, msg))
-
     def login(self):
-        self.log_msg("Open %s..." % Robot.LOGIN_URL)
+        self.logger.log("Open %s..." % Robot.LOGIN_URL)
         self.browser.get(Robot.LOGIN_URL)
         if self.debug > 1:
             self.browser.save_screenshot("debug1.png")
 
-        self.log_msg("Login...")
+        self.logger.log("Login...")
         ele_usr = self.browser.find_element_by_name("username")
         ele_pwd = self.browser.find_element_by_name("password")
         ele_usr.send_keys(self.username)
         ele_pwd.send_keys(self.password)
-        # self.browser.find_element_by_name("login").click()
+        self.browser.find_element_by_name("login").click()
         form = self.browser.find_element_by_id("clogs")
         form.submit()
         if self.debug > 1:
@@ -87,25 +109,26 @@ class Robot:
             host_name = host_link.text
             expiration_days = self.get_host_expiration_days(host, iteration)
 
-            self.log_msg(host_name + " expires in " + str(expiration_days) + " days")
+            self.logger.log(host_name + " expires in " + str(expiration_days) + " days")
             if expiration_days < 7:
                 self.update_host(host_link, host_name)
                 count += 1
             iteration += 1
-        self.browser.save_screenshot("result.png")
-        self.log_msg("Confirmed hosts: %d" % count, 2)
+            self.browser.save_screenshot("result.png")
+            TelegramSender.send_with_image(host_name + " updated", "result.png")
+        self.logger.log("Confirmed hosts: %d" % count, 2)
         return True
 
     def open_hosts_page(self):
-        self.log_msg("Open %s..." % Robot.HOST_URL)
+        self.logger.log("Open %s..." % Robot.HOST_URL)
         try:
             self.browser.get(Robot.HOST_URL)
         except TimeoutException as e:
             self.browser.save_screenshot("timeout.png")
-            self.log_msg("Timeout. Try to ignore")
+            self.logger.log("Timeout. Try to ignore")
 
     def update_host(self, host_link, host_name):
-        self.log_msg("Updating " + host_name)
+        self.logger.log("Updating " + host_name)
         host_link.click()
         time.sleep(3)
         self.browser.save_screenshot("modal_" + host_name + "_1.png")
@@ -134,14 +157,15 @@ class Robot:
 
     def run(self):
         rc = 0
-        self.log_msg("Debug level: %d" % self.debug)
+        self.logger.log("Debug level: %d" % self.debug)
         try:
             self.login()
             if not self.update_hosts():
                 rc = 3
         except Exception as e:
-            self.log_msg(str(e), 2)
+            self.logger.log(str(e))
             self.browser.save_screenshot("exception.png")
+            TelegramSender.send_with_image(str(e), "exception.png")
             rc = 2
         finally:
             self.browser.quit()
@@ -149,23 +173,23 @@ class Robot:
 
 
 def main(argv=None):
-    username, password, debug = get_args_values(argv)
-    return (Robot(username, password, debug)).run()
+    noip_username, noip_password, debug,  = get_args_values(argv)
+    return (Robot(noip_username, noip_password, debug)).run()
 
 
 def get_args_values(argv):
     if argv is None:
         argv = sys.argv
     if len(argv) < 3:
-        print("Usage: %s <username> <password> <num-hosts> [<debug-level>]" % argv[0])
+        print("Usage: %s <noip_username> <noip_password> [<debug-level>]" % argv[0])
         sys.exit(1)
 
-    username = argv[1]
-    password = argv[2]
+    noip_username = argv[1]
+    noip_password = argv[2]
     debug = 1
     if len(argv) > 3:
         debug = int(argv[3])
-    return username, password, debug
+    return noip_username, noip_password, debug
 
 
 if __name__ == "__main__":
--- noip-renew.sh
+++ noip-renew.sh
@@ -6,7 +6,8 @@ PASSWORD=""
 LOGDIR=$1
 PROGDIR=$(dirname $0)
 
-if [ -z "$LOGDIR" ]; then
+if [ -z "$LOGDIR" ]
+then
     $PROGDIR/noip-renew.py "$USERNAME" "$PASSWORD"
 else
     cd $LOGDIR
